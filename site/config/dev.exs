3cd7df6651515e0064b4f34d8b6b408fa94a8c25
Sasa Juric <sasa.juric@gmail.com>
2015-09-08 19:45:44 +0200

    always use the same port

--- site/config/dev.exs
+++ site/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :erlangelist, Erlangelist.Endpoint,
-  http: [port: 4000],
+  http: [port: 5454],
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
