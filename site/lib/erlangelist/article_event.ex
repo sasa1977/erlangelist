35225b9060bec9c26f4013809ab3dc59bb6d0152
Sasa Juric <sasa.juric@gmail.com>
2015-09-12 14:17:55 +0200

    test article events

--- site/lib/erlangelist/article_event.ex
+++ site/lib/erlangelist/article_event.ex
@@ -1,21 +1,23 @@
 defmodule Erlangelist.ArticleEvent do
+  def manager_name, do: :article_event
+
   def start_link do
-    res = GenEvent.start_link(name: :article_event)
+    res = GenEvent.start_link(name: manager_name)
     if match?({:ok, _}, res), do: install_handlers
     res
   end
 
   defp install_handlers do
     for event_handler <- Erlangelist.app_env!(:article_event_handlers) do
-      GenEvent.add_handler(:article_event, event_handler, nil)
+      GenEvent.add_mon_handler(manager_name, event_handler, nil)
     end
   end
 
   def visited(article, data) do
-    GenEvent.notify(:article_event, {:article_visited, article, data})
+    GenEvent.notify(manager_name, {:article_visited, article, data})
   end
 
   def invalid_article do
-    GenEvent.notify(:article_event, :invalid_article)
+    GenEvent.notify(manager_name, :invalid_article)
   end
 end
--- site/test/controllers/article_controller_test.exs
+++ site/test/controllers/article_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule Erlangelist.ArticleControllerTest do
   use Erlangelist.ConnCase
+
   alias Erlangelist.Article
+  alias Erlangelist.ArticleEvent
+  alias Erlangelist.EventTester
 
   test_get "/", 200, "<h1>#{Plug.HTML.html_escape(Article.most_recent.long_title)}</h1>"
 
@@ -12,5 +15,25 @@ defmodule Erlangelist.ArticleControllerTest do
     if article.legacy_url do
       test_get article.legacy_url, 200, "<h1>#{Plug.HTML.html_escape(article.long_title)}</h1>"
     end
+  end
+
+  test "article visited event" do
+    article_id = Article.most_recent.id
+    EventTester.start_listener(ArticleEvent.manager_name)
+    get(conn, "/article/#{article_id}")
+    assert_receive(
+      {:event,
+        {:article_visited,
+          %{exists?: true, id: ^article_id},
+          %{remote_ip: "127.0.0.1"}
+        }
+      }
+    )
+  end
+
+  test "unknown article event" do
+    EventTester.start_listener(ArticleEvent.manager_name)
+    get(conn, "/article/unknown")
+    assert_receive {:event, :invalid_article}
   end
 end
