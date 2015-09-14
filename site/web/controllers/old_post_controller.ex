12dce01bdcae40bd6410a264dcc625c34c64bd1c
Sasa Juric <sasa.juric@gmail.com>
2015-09-14 21:25:08 +0200

    fix old post controller

--- site/web/controllers/old_post_controller.ex
+++ site/web/controllers/old_post_controller.ex
@@ -1,6 +1,8 @@
 defmodule Erlangelist.OldPostController do
   use Erlangelist.Web, :controller
 
+  alias Erlangelist.ArticleEvent
+
   def render(%{private: %{article: article}} = conn, _params) do
     ArticleEvent.visited(article, %{remote_ip: remote_ip_string(conn)})
     redirect(conn, external: "http://theerlangelist.blogspot.com#{conn.request_path}")
