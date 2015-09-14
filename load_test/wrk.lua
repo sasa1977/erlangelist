4e17070dbfdc65ceccb3aa71282c8e11c635cec6
Sasa Juric <sasa.juric@gmail.com>
2015-09-14 21:36:47 +0200

    basic wrk script

---
+++ load_test/wrk.lua
@@ -0,0 +1,20 @@
+-- Simple test wrk script. Run as:
+--   wrk -t10 -c10 -d60s --timeout 1000 -s wrk.lua "http://IP"
+
+requests = {
+  "/article/outside_elixir",
+  "/article/tallakt_macros",
+  "/article/beyond_taskasync",
+  "/",
+  "/2015/07/beyond-taskasync.html",
+  "/2013/01/teaching-orthogonal-programming.html",
+  "/invalid_url",
+  "/article/invalid_article"
+}
+
+length = #requests
+
+request = function()
+  wrk.method = "GET"
+  return wrk.format(nil, requests[math.random(length)])
+end
