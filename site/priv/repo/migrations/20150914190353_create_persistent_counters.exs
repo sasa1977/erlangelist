a7dae887774675dae3f7d257142c5272d16c7b23
Sasa Juric <sasa.juric@gmail.com>
2015-09-19 20:07:03 +0200

    don't use inherited tables

--- site/priv/repo/migrations/20150914190353_create_persistent_counters.exs
+++ site/priv/repo/migrations/20150914190353_create_persistent_counters.exs
@@ -2,21 +2,20 @@ defmodule Erlangelist.Repo.Migrations.CreatePersistentCounters do
   use Ecto.Migration
 
   def change do
-    create table(:persistent_counters) do
-      add :key, :text, null: false
-      add :value, :bigint, null: false
-      add :created_at, :datetime,
-        null: false,
-        default: fragment("(now() at time zone 'utc')")
-    end
-
     for table <- [
       :article_visits,
       :country_visits,
       :referer_host_visits,
       :referer_visits
     ] do
-      create table(table, options: "inherits(persistent_counters)")
+      create table(table) do
+        add :key, :text, null: false
+        add :value, :bigint, null: false
+        add :created_at, :datetime,
+          null: false,
+          default: fragment("(now() at time zone 'utc')")
+      end
+
       create index(table, [:key], unique: false)
       create index(table, [:created_at], unique: false)
     end
