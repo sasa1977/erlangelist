4282c8a486be22b5036c4cd11fa69e5443d45fb0
Sasa Juric <sasa.juric@gmail.com>
2015-10-22 19:52:44 +0200

    remove needless files

--- site/config/config.exs
+++ site/config/config.exs
@@ -13,7 +13,3 @@ for {app, settings} <- Erlangelist.Settings.all do
 end
 
 import_config "exometer.exs"
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
