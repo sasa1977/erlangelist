e31cf923c384e1ee9cd1604a90186b34c66e7d32
Sasa Juric <sasa.juric@gmail.com>
2015-10-24 13:31:43 +0200

    ssh tunnel

---
+++ provisioning/ssh-tunnel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o pipefail
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+machine="$1"
+
+if [ "$machine" == "" ]; then
+  printf "\nSyntax:\n\n  ${BASH_SOURCE[0]} target_machine\n\n"
+  exit 1
+fi
+
+settings=$(ssh $machine "cat /opt/erlangelist/erlangelist-settings.sh")
+eval $settings
+
+echo "
+admin site: 127.0.0.1:$ERLANGELIST_ADMIN_HTTP_PORT
+database:   127.0.0.1:$ERLANGELIST_POSTGRES_PORT
+graphite:   127.0.0.1:$ERLANGELIST_GRAPHITE_NGINX_PORT
+"
+
+ssh \
+  -L $ERLANGELIST_ADMIN_HTTP_PORT:127.0.0.1:$ERLANGELIST_ADMIN_HTTP_PORT \
+  -L $ERLANGELIST_POSTGRES_PORT:127.0.0.1:$ERLANGELIST_POSTGRES_PORT \
+  -L $ERLANGELIST_GRAPHITE_NGINX_PORT:127.0.0.1:$ERLANGELIST_GRAPHITE_NGINX_PORT \
+  $1 "sleep infinity"
