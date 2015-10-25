52542efa737417fd0705e82b061ffd48dd04a06a
Sasa Juric <sasa.juric@gmail.com>
2015-10-25 21:55:08 +0100

    log ip address

--- site/web/plugs/logger.ex
+++ site/web/plugs/logger.ex
@@ -1,14 +1,26 @@
 defmodule Erlangelist.Plug.Logger do
   @behaviour Plug
 
+  require Logger
+
   def init(opts) do
     Plug.Logger.init(opts)
   end
 
   def call(conn, level) do
-    case Erlangelist.run_limited(:plug_logger, fn -> Plug.Logger.call(conn, level) end) do
-      {:ok, result} -> result
+    case Erlangelist.run_limited(:plug_logger, fn -> do_log(conn, level) end) do
+      {:ok, new_conn} -> new_conn
       :error -> conn
     end
+  end
+
+  defp do_log(conn, level) do
+    conn = Plug.Logger.call(conn, level)
+
+    Logger.log level, fn ->
+      ["from: ", Erlangelist.Helper.ip_string(conn.remote_ip) || "unknown"]
+    end
+
+    conn
   end
 end
