f527ecdde188bdb5d4b4f86138b756986b0031d2
Sasa Juric <sasa.juric@gmail.com>
2016-02-20 16:46:20 +0100

    hello world

--- examples/simple_server/lib/simple_server/endpoint.ex
+++ examples/simple_server/lib/simple_server/endpoint.ex
@@ -1,3 +1,9 @@
 defmodule SimpleServer.Endpoint do
   use Phoenix.Endpoint, otp_app: :simple_server
+
+  plug :render
+
+  def render(conn, _opts) do
+    Plug.Conn.send_resp(conn, 200, "Hello World!")
+  end
 end
