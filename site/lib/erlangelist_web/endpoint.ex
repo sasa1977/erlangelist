a81834e8fd11286d6027b5acbb3cc08ae0e28dbd
Sasa Juric <sasa.juric@gmail.com>
2018-05-10 18:53:33 +0200

    use properly formed local domain

--- site/lib/erlangelist_web/endpoint.ex
+++ site/lib/erlangelist_web/endpoint.ex
@@ -47,7 +47,7 @@ defmodule ErlangelistWeb.Endpoint do
     %{
       run_client?: unquote(Mix.env() != :test),
       ca_url: os_setting("CA_URL", "http://localhost:4000/directory"),
-      domain: os_setting("DOMAIN", "localhost"),
+      domain: os_setting("DOMAIN", "local.host"),
       extra_domains: os_setting("EXTRA_DOMAINS", "") |> String.split(",") |> Enum.reject(&(&1 == "")),
       email: os_setting("EMAIL", "mail@foo.bar"),
       base_folder: Path.join(Application.app_dir(:erlangelist, "priv"), "certbot"),
