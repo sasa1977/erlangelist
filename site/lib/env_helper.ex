a1bea320494e00132861a8b70886dec5b8c0766b
Sasa Juric <sasa.juric@gmail.com>
2018-05-21 17:44:58 +0200

    improve env helper

--- site/lib/env_helper.ex
+++ site/lib/env_helper.ex
@@ -1,7 +1,7 @@
 defmodule EnvHelper do
   defmacro env_specific(config) do
     quote do
-      unquote(Keyword.get(config, Mix.env(), Keyword.get(config, :else)))
+      unquote(Keyword.get_lazy(config, Mix.env(), fn -> Keyword.fetch!(config, :else) end))
     end
   end
 end
