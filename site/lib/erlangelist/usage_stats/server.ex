303a269adb1119bf12ca68c7403895bd333282bf
Sasa Juric <sasa.juric@gmail.com>
2018-08-18 19:39:26 +0200

    improve stats storage

--- site/lib/erlangelist/usage_stats/server.ex
+++ site/lib/erlangelist/usage_stats/server.ex
@@ -4,25 +4,54 @@ defmodule Erlangelist.UsageStats.Server do
 
   def start_link(_), do: Parent.GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 
-  def report(key, value), do: GenServer.cast(__MODULE__, {:report, key, value})
+  def report(category, subcategory), do: GenServer.cast(__MODULE__, {:report, category, subcategory})
 
   @impl GenServer
-  def init(_), do: {:ok, []}
+  def init(_), do: {:ok, read_stats()}
 
   @impl GenServer
-  def handle_cast({:report, key, value}, stats), do: {:noreply, add_report(stats, key, value)}
+  def handle_cast({:report, category, subcategory}, stats),
+    do: {:noreply, stats |> inc(category, subcategory) |> maybe_start_writer()}
 
   @impl Parent.GenServer
-  def handle_child_terminated(UsageStats.Writer, _meta, _pid, _reason, stats), do: {:noreply, maybe_start_writer(stats)}
-
-  defp add_report(stats, key, value), do: maybe_start_writer([{key, value} | stats])
+  def handle_child_terminated(UsageStats.Writer, changes, _pid, reason, stats) do
+    stats = if reason != :normal, do: update_in(stats.changes, &MapSet.union(&1, changes)), else: stats
+    {:noreply, maybe_start_writer(clear_old_stats(stats))}
+  end
 
   defp maybe_start_writer(stats) do
-    if not Parent.GenServer.child?(UsageStats.Writer) and not Enum.empty?(stats) do
-      Parent.GenServer.start_child({UsageStats.Writer, stats})
-      []
+    if not Enum.empty?(stats.changes) and not Parent.GenServer.child?(UsageStats.Writer) do
+      Parent.GenServer.start_child({UsageStats.Writer, changed_data(stats)})
+      %{stats | changes: MapSet.new()}
     else
       stats
     end
   end
+
+  defp read_stats() do
+    today = Date.utc_today()
+    %{changes: MapSet.new(), data: %{today => UsageStats.Writer.stored_data(today)}}
+  end
+
+  defp clear_old_stats(stats) do
+    dates_to_clear =
+      Map.keys(stats.data)
+      |> Enum.sort()
+      |> Enum.reverse()
+      |> Enum.drop(1)
+      |> MapSet.new()
+      |> MapSet.difference(stats.changes)
+
+    update_in(stats.data, &Map.drop(&1, dates_to_clear))
+  end
+
+  defp inc(stats, category, subcategory) do
+    today = Date.utc_today()
+    %{stats | data: do_inc(stats.data, [today, category, subcategory]), changes: MapSet.put(stats.changes, today)}
+  end
+
+  defp changed_data(stats), do: Enum.map(stats.changes, &{&1, Map.fetch!(stats.data, &1)})
+
+  defp do_inc(map, [final_key]), do: map |> Map.put_new(final_key, 0) |> Map.update!(final_key, &(&1 + 1))
+  defp do_inc(map, [this_key | other_keys]), do: Map.put(map, this_key, do_inc(Map.get(map, this_key, %{}), other_keys))
 end
--- site/lib/erlangelist/usage_stats/writer.ex
+++ site/lib/erlangelist/usage_stats/writer.ex
@@ -5,22 +5,9 @@ defmodule Erlangelist.UsageStats.Writer do
   def child_spec(stats) do
     %{
       id: __MODULE__,
-      start: {Task, :start_link, [fn -> write!(stats) end]}
+      start: {Task, :start_link, [fn -> write!(stats) end]},
+      meta: stats |> Enum.map(fn {date, _data} -> date end) |> MapSet.new()
     }
-  end
-
-  defp write!(stats) do
-    date = Date.utc_today()
-    data_to_store = Enum.reduce(stats, stored_data(date), fn {key, value}, data -> inc_counter(data, key, value) end)
-    File.write!(date_file(date), :erlang.term_to_binary(data_to_store))
-    Erlangelist.Backup.backup(UsageStats.folder())
-  end
-
-  defp inc_counter(data, key, value) do
-    data
-    |> Map.put_new(key, %{})
-    |> update_in([key], &Map.put_new(&1, value, 0))
-    |> update_in([key, value], &(&1 + 1))
   end
 
   def stored_data(date) do
@@ -32,6 +19,12 @@ defmodule Erlangelist.UsageStats.Writer do
     catch
       _, _ -> %{}
     end
+  end
+
+  defp write!(stats) do
+    Enum.each(stats, fn {date, data} -> File.write!(date_file(date), :erlang.term_to_binary(data)) end)
+
+    Erlangelist.Backup.backup(UsageStats.folder())
   end
 
   defp date_file(date), do: Path.join(UsageStats.folder(), Date.to_iso8601(date, :basic))
