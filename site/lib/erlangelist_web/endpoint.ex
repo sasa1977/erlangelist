9759ec558ae2f9399396a511b8a498a1826316e8
Saša Jurić <sasa.juric@gmail.com>
2020-05-03 23:45:02 +0200

    Improve formatting

--- site/lib/erlangelist_web/endpoint.ex
+++ site/lib/erlangelist_web/endpoint.ex
@@ -1,32 +1,30 @@
 defmodule ErlangelistWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :erlangelist
 
-  plug(Plug.Static, at: "/", from: :erlangelist, gzip: false, only: ~w(css fonts images js favicon.ico robots.txt))
+  plug Plug.Static, at: "/", from: :erlangelist, gzip: false, only: ~w(css fonts images js favicon.ico robots.txt)
 
   if code_reloading? do
-    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
-    plug(Phoenix.LiveReloader)
-    plug(Phoenix.CodeReloader)
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
   end
 
-  plug(Plug.Logger, log: :debug)
-  plug(SiteEncrypt.AcmeChallenge, ErlangelistWeb.Site)
+  plug Plug.Logger, log: :debug
+  plug SiteEncrypt.AcmeChallenge, ErlangelistWeb.Site
 
   unless Mix.env() == :test, do: plug(ErlangelistWeb.ForceSSL)
 
-  plug(
-    Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Jason
-  )
 
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
+  plug Plug.MethodOverride
+  plug Plug.Head
 
-  plug(ErlangelistWeb.MovePermanently, from: "theerlangelist.com", to: "www.theerlangelist.com")
+  plug ErlangelistWeb.MovePermanently, from: "theerlangelist.com", to: "www.theerlangelist.com"
 
-  plug(ErlangelistWeb.Router)
+  plug ErlangelistWeb.Router
 
   def init(_key, phoenix_defaults), do: {:ok, ErlangelistWeb.EndpointConfig.config(phoenix_defaults)}
 end
