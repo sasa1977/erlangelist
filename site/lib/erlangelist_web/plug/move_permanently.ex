5c9bbd732966350058a96128886f930ae66a8733
Saša Jurić <sasa.juric@gmail.com>
2020-05-04 18:40:48 +0200

    Condense code

--- site/lib/erlangelist_web/plug/move_permanently.ex
+++ site/lib/erlangelist_web/plug/move_permanently.ex
@@ -5,17 +5,15 @@ defmodule ErlangelistWeb.Plug.MovePermanently do
   def init(opts), do: opts |> Keyword.take([:from, :to]) |> Map.new()
 
   @impl Plug
-  def call(conn, opts) do
-    if conn.host == opts.from do
-      url = "#{conn.scheme}://#{opts.to}#{conn.request_path}"
+  def call(conn, %{from: from, to: to}) do
+    with %{host: ^from} <- conn do
+      url = "#{conn.scheme}://#{to}#{conn.request_path}"
       body = "<html><body>The document has moved <a href=\"#{url}\">here</a>.</body></html>"
 
       conn
       |> Plug.Conn.put_resp_header("location", url)
       |> Plug.Conn.send_resp(:moved_permanently, body)
       |> Plug.Conn.halt()
-    else
-      conn
     end
   end
 end
