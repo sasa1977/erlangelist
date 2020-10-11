b597a5362ae8a6fa29e5d1fe8c03d70f66ea3ac8
Saša Jurić <sasa.juric@gmail.com>
2020-10-11 15:38:16 +0200

    Simplify db_path

--- site/lib/erlangelist/config.ex
+++ site/lib/erlangelist/config.ex
@@ -19,8 +19,8 @@ defmodule Erlangelist.Config do
   def backup_folder, do: priv_path("backup")
   def usage_stats_folder, do: db_path("usage_stats")
 
-  def db_path(parts) when is_list(parts), do: Path.join([Application.app_dir(:erlangelist, "priv"), "db" | parts])
-  def db_path(name), do: db_path([name])
+  def db_path, do: Path.join(Application.app_dir(:erlangelist, "priv"), "db")
+  def db_path(name), do: Path.join(db_path(), name)
 
   defp priv_path(parts) when is_list(parts), do: Path.join([Application.app_dir(:erlangelist, "priv") | parts])
   defp priv_path(name), do: priv_path([name])
